04cf4ccb109f8e3fe5ae3514d47560e3a5621d5b
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-01-28 14:15:12 +0200

    Added source and public folders.

---
+++ Project/public/List.js
@@ -0,0 +1,4 @@
+const form = document.querySelector(".form_container");
+const type = document.querySelector("#to_from");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
---
+++ Project/src/List.ts
@@ -0,0 +1,4 @@
+const form = document.querySelector(".form_container") as HTMLFormElement;
+const type = document.querySelector("#to_from") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
