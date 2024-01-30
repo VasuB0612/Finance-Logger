148a641c71786d4e3bbdb99305ee43bb4e031a40
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-01-30 15:53:23 +0200

    Added a class for Invoices.

--- Project/public/List.js
+++ Project/public/List.js
@@ -1,4 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Invoices_1 = require("./Classes/Invoices");
 const form = document.querySelector(".form_container");
 const type = document.querySelector("#to_from");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const invoice1 = new Invoices_1.Invoice("Alan", "work on the website", 500);
+const invoice2 = new Invoices_1.Invoice("Charlie", "work on the website", 400);
+const invoices = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+invoices.forEach((inv) => {
+    console.log(inv);
+});
--- Project/src/List.ts
+++ Project/src/List.ts
@@ -1,4 +1,17 @@
+import { Invoice } from "./Classes/Invoices";
+
 const form = document.querySelector(".form_container") as HTMLFormElement;
 const type = document.querySelector("#to_from") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+const invoice1 = new Invoice("Alan", "work on the website", 500);
+const invoice2 = new Invoice("Charlie", "work on the website", 400);
+
+const invoices: Invoice[] = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+
+invoices.forEach((inv) => {
+  console.log(inv);
+});
